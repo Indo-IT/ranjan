eb46f9ae2f948f1ee5665ecaf302142491325d54
Ranjan <67844145+Indo-IT@users.noreply.github.com>
2021-01-30 18:51:31 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(function() {
-  // jQuery^Project logo 
+// jQuery^Project logo 
   $('.lesson-hover').hover(
     function() {
-      // 子要素の「.text-contents」の要素を取得し、text-activeクラスをつけてください      
+      // 「.text-contents」、text-active      
       $(this).find('.text-contents').addClass('text-active');
     },
     function() {
@@ -10,7 +10,44 @@ $(function() {
       
     }
   );
+  
+  // jQuery^ header manu navigation
+  $('header a').click(function() {
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html, body').animate({
+      'scrollTop': position
+    }, 500);
+  });
+  
+  // jQuery^ top-logo navigation
+  $('.top-logo').click(function(){
+    $('html, body').animate({
+      'scrollTop': 0
+    }, 300);
+  });
+  
+    // jQuery^ certificate sliding
+  
+  function toggleChangeBtn() {
+    var slideIndex = $('.slide').index($('.active'));
+    $('.change-btn').show();
+    if (slideIndex == 0) {
+      $('.prev-btn').hide();
+    // 「3」の部分を、lengthメソッドを用いて書き換えてください
+    } else if (slideIndex == $('.slide').length - 1) {
+      $('.next-btn').hide();
+    }
+  }
+  
+  $('.index-btn').click(function() {
+    $('.active').removeClass('active');
+    var clickedIndex = $('.index-btn').index($(this));
+    $('.slide').eq(clickedIndex).addClass('active');
+    toggleChangeBtn();
+  });
+  
+
+  
 });
 
-// ready^Project logo 
-  
